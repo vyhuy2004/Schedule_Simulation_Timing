75303ce77682d8668ce1c003718d0b4df3cd1c1f
Huy_Nguyen_97 <47517539+vyhuy2004@users.noreply.github.com>
2019-04-30 01:13:46 -0700

    Add files via upload
    v2
    //STCF supported

--- scheduler.cpp
+++ scheduler.cpp
@@ -14,11 +14,17 @@ struct job
 	int finish_time = 0;
 	int remaining_time = 0;
 	bool running = false;
+	bool ready = false;
 };
 
 bool compbyarrival(const job& a, const job& b)
 {
 	return a.job_arrival < b.job_arrival;
+}
+
+bool compbylength(const job& a, const job& b)
+{
+	return a.remaining_time < b.remaining_time;
 }
 
 bool compbySJF(const job& a, const job& b)
@@ -41,6 +47,10 @@ bool compbyBJF(const job& a, const job& b)
 	return a.job_arrival < b.job_arrival;
 }
 
+bool operator==(const job& a, const job& b)
+{
+    return a.job_id == b.job_id;
+}
 int main()
 {
 	std::vector<job> listofjob;
@@ -238,6 +248,100 @@ int main()
 		std::cout << listofjob[i].finish_time << std::endl;
 	} 
 
+
+	//STCF
+	//Reset values
+	clock = 0;
+	finished_task = 0;
+	done = false;
+	for(int i = 0; i < counter; i++)
+	{
+		listofjob[i].start_time = 0;
+		listofjob[i].finish_time = 0;
+		listofjob[i].remaining_time = listofjob[i].job_length;
+	}
+ 	//Initialize new vector
+ 	std::vector<job> runningQ;
+ 	std::vector<job> readyQ;
+ 	int runningtask = 0;
+ 	int readytask = 0;
+	//Resort vector
+	std::sort(listofjob.begin(), listofjob.end(), compbySJF);
+	while(done == false)
+	{
+		//Set to ready
+		for(int i = 0; i < counter; i++)
+		{
+			if (listofjob[i].job_arrival == clock)
+			{
+				listofjob[i].ready = true;
+				readyQ.push_back(listofjob[i]);
+				readytask++;
+				std::sort(readyQ.begin(), readyQ.end(), compbylength);
+			}
+		}
+		if(!readyQ.empty())
+		{
+			//Get current running task
+			for(int i = 0; i < counter; i++)
+			{
+				if(listofjob[i] == readyQ[0])
+				{
+					runningtask = i;
+					break;
+				}
+			}
+			//Run task
+			if(listofjob[runningtask].running == false && ((listofjob[runningtask].remaining_time == listofjob[runningtask].job_length && listofjob[runningtask].job_arrival == 0) || (listofjob[runningtask].job_arrival != 0)))
+			{
+				listofjob[runningtask].start_time = clock;	
+				listofjob[runningtask].running = true;
+			}
+			if(listofjob[runningtask].remaining_time >= 0)
+			{
+				listofjob[runningtask].remaining_time--;
+				readyQ[0].remaining_time--;
+				if(listofjob[runningtask].remaining_time == 0)
+				{
+					listofjob[runningtask].finish_time = clock + 1;
+					listofjob[runningtask].running = false;
+					readyQ.erase(readyQ.begin());
+					readytask--;
+					std::sort(readyQ.begin(), readyQ.end(), compbylength);
+					finished_task++;
+					if((!readyQ.empty()) && clock >= readyQ[0].job_arrival)
+					{
+						for(int i = 0; i < counter; i++)
+						{
+							if(listofjob[i] == readyQ[0])
+							{
+								runningtask = i;
+								break;
+							}
+						}
+						if(listofjob[runningtask].job_arrival != 0)
+						{
+							listofjob[runningtask].start_time = clock+1;
+						}	
+						listofjob[runningtask].running = true;
+					}					
+				}
+			}
+		}
+		listofjob[finished_task].running = true;
+		if(finished_task == counter)
+		{
+			done = true;
+		}
+		clock++;
+	}
+	std::cout<< std::endl << "STCF: " << std::endl;
+	for(int i = 0; i < counter; i++)
+	{
+		std::cout << listofjob[i].job_id<< " ";
+		std::cout << listofjob[i].start_time << " ";
+		std::cout << listofjob[i].finish_time << std::endl;
+	} 
 
 	return 0;
 }
