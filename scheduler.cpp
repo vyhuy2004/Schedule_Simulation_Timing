6af152d59daf39aee16279e483aa9b95d5896979
Huy_Nguyen_97 <47517539+vyhuy2004@users.noreply.github.com>
2019-04-29 18:04:21 -0700

    Add files via upload
    v1
    //Added support for FIFO, SJF, BJF
    //Fix weird bug with FIFO in v0

--- scheduler.cpp
+++ scheduler.cpp
@@ -13,6 +13,7 @@ struct job
 	int start_time = 0;
 	int finish_time = 0;
 	int remaining_time = 0;
+	bool running = false;
 };
 
 bool compbyarrival(const job& a, const job& b)
@@ -55,10 +56,6 @@ int main()
 		element >> testjob.job_length;
 		testjob.remaining_time =  testjob.job_length;
 		listofjob.push_back(testjob);
-		std::cout << listofjob[counter].job_id<< " ";
-		std::cout << listofjob[counter].job_arrival << " ";
-		std::cout << listofjob[counter].job_length << " ";
-		std::cout << listofjob[counter].remaining_time << std::endl;
 		counter++;
 	}
 	std::cout << std::endl;
@@ -70,31 +67,41 @@ int main()
 		std::cout << listofjob[i].job_arrival << " ";
 		std::cout << listofjob[i].job_length << std::endl;
 	} 
-	std::cout << std::endl;
+	
 	//FIFO no preemption
 	int clock = 0;
 	int finished_task = 0;
 	bool done = false;
 	while(done == false)
 	{
-		//std::cout <<"Start";
 		if(listofjob[finished_task].start_time == 0 && clock == 0)
 		{
-			listofjob[finished_task].start_time = 0;	
+			listofjob[finished_task].start_time = 0;
+			listofjob[finished_task].running = true;	
 		}
-		else if(listofjob[finished_task].start_time == 0 && finished_task != 0)
+		else if(listofjob[finished_task].start_time == 0 && finished_task != 0 && clock >= listofjob[finished_task].job_arrival)
 		{
-			listofjob[finished_task].start_time = clock - 1;	
-
+			listofjob[finished_task].start_time = clock;	
+			listofjob[finished_task].running = true;
 		}
-		listofjob[finished_task].finish_time = clock; 
-		if(listofjob[finished_task].remaining_time != 0)
+		if(listofjob[finished_task].running == true)
 		{
-			listofjob[finished_task].remaining_time--;
-		}
-		else
-		{
-			finished_task++;
+			
+			if(listofjob[finished_task].remaining_time >= 0)
+			{
+				listofjob[finished_task].remaining_time--;
+				if(listofjob[finished_task].remaining_time == 0)
+				{
+					listofjob[finished_task].finish_time = clock + 1;
+					listofjob[finished_task].running = false;
+					finished_task++;
+					if(clock >= listofjob[finished_task].job_arrival)
+					{
+						listofjob[finished_task].start_time = clock+1;	
+						listofjob[finished_task].running = true;
+					}					
+				}
+			}
 		}
 		if(finished_task == counter)
 		{
@@ -103,12 +110,134 @@ int main()
 		clock++;
 	}
 	
-	
+	std::cout<< std::endl << "FIFO: " << std::endl;
 	for(int i = 0; i < counter; i++)
 	{
 		std::cout << listofjob[i].job_id<< " ";
 		std::cout << listofjob[i].start_time << " ";
 		std::cout << listofjob[i].finish_time << std::endl;
 	} 
+	
+	//SJF no preemption
+	//Reset values
+	clock = 0;
+	finished_task = 0;
+	done = false;
+	for(int i = 0; i < counter; i++)
+	{
+		listofjob[i].start_time = 0;
+		listofjob[i].finish_time = 0;
+		listofjob[i].remaining_time = listofjob[i].job_length;
+	}
+ 
+	//Resort vector
+	std::sort(listofjob.begin(), listofjob.end(), compbySJF);
+ 
+	while(done == false)
+	{
+		if(listofjob[finished_task].start_time == 0 && clock == 0)
+		{
+			listofjob[finished_task].start_time = 0;
+			listofjob[finished_task].running = true;	
+		}
+		else if(listofjob[finished_task].start_time == 0 && finished_task != 0 && clock >= listofjob[finished_task].job_arrival)
+		{
+			listofjob[finished_task].start_time = clock;	
+			listofjob[finished_task].running = true;
+		}
+		if(listofjob[finished_task].running == true)
+		{
+			
+			if(listofjob[finished_task].remaining_time >= 0)
+			{
+				listofjob[finished_task].remaining_time--;
+				if(listofjob[finished_task].remaining_time == 0)
+				{
+					listofjob[finished_task].finish_time = clock + 1;
+					listofjob[finished_task].running = false;
+					finished_task++;
+					if(clock >= listofjob[finished_task].job_arrival)
+					{
+						listofjob[finished_task].start_time = clock+1;	
+						listofjob[finished_task].running = true;
+					}					
+				}
+			}
+		}
+		if(finished_task == counter)
+		{
+			done = true;
+		}
+		clock++;
+	}
+
+	std::cout<< std::endl << "SJF: " << std::endl;
+	for(int i = 0; i < counter; i++)
+	{
+		std::cout << listofjob[i].job_id<< " ";
+		std::cout << listofjob[i].start_time << " ";
+		std::cout << listofjob[i].finish_time << std::endl;
+	} 
+
+	//BJF no preemption
+	//Reset values
+	clock = 0;
+	finished_task = 0;
+	done = false;
+	for(int i = 0; i < counter; i++)
+	{
+		listofjob[i].start_time = 0;
+		listofjob[i].finish_time = 0;
+		listofjob[i].remaining_time = listofjob[i].job_length;
+	} 
+	//Resort vector
+	std::sort(listofjob.begin(), listofjob.end(), compbyBJF);
+	while(done == false)
+	{
+		if(listofjob[finished_task].start_time == 0 && clock == 0)
+		{
+			listofjob[finished_task].start_time = 0;
+			listofjob[finished_task].running = true;	
+		}
+		else if(listofjob[finished_task].start_time == 0 && finished_task != 0 && clock >= listofjob[finished_task].job_arrival)
+		{
+			listofjob[finished_task].start_time = clock;	
+			listofjob[finished_task].running = true;
+		}
+		if(listofjob[finished_task].running == true)
+		{
+			
+			if(listofjob[finished_task].remaining_time >= 0)
+			{
+				listofjob[finished_task].remaining_time--;
+				if(listofjob[finished_task].remaining_time == 0)
+				{
+					listofjob[finished_task].finish_time = clock + 1;
+					listofjob[finished_task].running = false;
+					finished_task++;
+					if(clock >= listofjob[finished_task].job_arrival)
+					{
+						listofjob[finished_task].start_time = clock+1;	
+						listofjob[finished_task].running = true;
+					}					
+				}
+			}
+		}
+		if(finished_task == counter)
+		{
+			done = true;
+		}
+		clock++;
+	}
+
+	std::cout<< std::endl << "BJF: " << std::endl;
+	for(int i = 0; i < counter; i++)
+	{
+		std::cout << listofjob[i].job_id<< " ";
+		std::cout << listofjob[i].start_time << " ";
+		std::cout << listofjob[i].finish_time << std::endl;
+	} 
+
+
 	return 0;
 }
